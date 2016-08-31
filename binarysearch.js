60dac81785c77e36e5ce3b8659310a925a737c6f
Ryan Rotz <ryanrotz@gmail.com>
2016-08-31 13:46:26 -0500

    first commit, starting binary search

---
+++ binarysearch.js
@@ -0,0 +1,21 @@
+
+myArr = [1, 4, 6, 9, 11, 15, 18, 20, 21, 22, 30, 40, 44, 55, 70]
+
+function binarySearch(arr, num) {
+  var min = 0;
+  var max = arr.length-1;
+  var index; 
+  var element;
+
+  while(max >= min) {
+    index = 
+
+
+
+
+
+
+  }
+
+  return -1;
+}
