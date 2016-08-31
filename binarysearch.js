2bbfab89a5f79f17f7639ae7ce437e1170c2f37a
Ryan Rotz <ryanrotz@gmail.com>
2016-08-31 14:03:31 -0500

    binary search working

--- binarysearch.js
+++ binarysearch.js
@@ -3,19 +3,24 @@ myArr = [1, 4, 6, 9, 11, 15, 18, 20, 21, 22, 30, 40, 44, 55, 70]
 
 function binarySearch(arr, num) {
   var min = 0;
-  var max = arr.length-1;
+  var max = arr.length - 1;
   var index; 
   var element;
 
-  while(max >= min) {
-    index = 
+  while(min <= max) {
+    index = Math.floor((max - min) / 2);
+    element = arr[index];
 
-
-
-
-
-
+    if(element < num) {
+      min = index + 1;
+    } else if(element > num) {
+        max = index - 1;
+    } else {
+        return index;
+    }
   }
 
   return -1;
 }
+
+console.log(binarySearch(myArr, 9));
